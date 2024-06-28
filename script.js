523e050d0c916d80374e0bd60032f4f4469583d3
daviddvorszky <dv98david@gmail.com>
2024-06-28 20:44:08 +0200

    Refactor code to better follow es6 standards

--- script.js
+++ script.js
@@ -26,82 +26,78 @@ const villageTypes = Object.freeze({
     wheat_6_iron_no_clay: 11,
 });
 
-// The order must be the same as the indices in the villageTypes object
 const buildingLimits = [
-    { 'Bakery': 1, 'Brickyard': 1, 'Clay Pit': 1, 'Cropland': 15, 'Grain Mill': 1, 'Iron Foundry': 1, 'Iron Mine': 1, 'Sawmill': 1, 'Woodcutter': 1, 'Hero\'s Mansion': 1 },
-    { 'Bakery': 1, 'Brickyard': 1, 'Clay Pit': 3, 'Cropland': 9, 'Grain Mill': 1, 'Iron Foundry': 1, 'Iron Mine': 3, 'Sawmill': 1, 'Woodcutter': 3, 'Hero\'s Mansion': 1 },
-    { 'Bakery': 1, 'Brickyard': 1, 'Clay Pit': 4, 'Cropland': 7, 'Grain Mill': 1, 'Iron Foundry': 1, 'Iron Mine': 4, 'Sawmill': 1, 'Woodcutter': 3, 'Hero\'s Mansion': 1 },
-    { 'Bakery': 1, 'Brickyard': 1, 'Clay Pit': 3, 'Cropland': 7, 'Grain Mill': 1, 'Iron Foundry': 1, 'Iron Mine': 4, 'Sawmill': 1, 'Woodcutter': 4, 'Hero\'s Mansion': 1 },
-    { 'Bakery': 1, 'Brickyard': 1, 'Clay Pit': 4, 'Cropland': 7, 'Grain Mill': 1, 'Iron Foundry': 1, 'Iron Mine': 3, 'Sawmill': 1, 'Woodcutter': 4, 'Hero\'s Mansion': 1 },
-    { 'Bakery': 1, 'Brickyard': 1, 'Clay Pit': 4, 'Cropland': 6, 'Grain Mill': 1, 'Iron Foundry': 1, 'Iron Mine': 4, 'Sawmill': 1, 'Woodcutter': 4, 'Hero\'s Mansion': 1 },
-    { 'Bakery': 1, 'Brickyard': 1, 'Clay Pit': 3, 'Cropland': 6, 'Grain Mill': 1, 'Iron Foundry': 1, 'Iron Mine': 4, 'Sawmill': 1, 'Woodcutter': 5, 'Hero\'s Mansion': 1 },
-    { 'Bakery': 1, 'Brickyard': 1, 'Clay Pit': 4, 'Cropland': 6, 'Grain Mill': 1, 'Iron Foundry': 1, 'Iron Mine': 3, 'Sawmill': 1, 'Woodcutter': 5, 'Hero\'s Mansion': 1 },
-    { 'Bakery': 1, 'Brickyard': 1, 'Clay Pit': 5, 'Cropland': 6, 'Grain Mill': 1, 'Iron Foundry': 1, 'Iron Mine': 4, 'Sawmill': 1, 'Woodcutter': 3, 'Hero\'s Mansion': 1 },
-    { 'Bakery': 1, 'Brickyard': 1, 'Clay Pit': 5, 'Cropland': 6, 'Grain Mill': 1, 'Iron Foundry': 1, 'Iron Mine': 3, 'Sawmill': 1, 'Woodcutter': 4, 'Hero\'s Mansion': 1 },
-    { 'Bakery': 1, 'Brickyard': 1, 'Clay Pit': 4, 'Cropland': 6, 'Grain Mill': 1, 'Iron Foundry': 1, 'Iron Mine': 5, 'Sawmill': 1, 'Woodcutter': 3, 'Hero\'s Mansion': 1 },
-    { 'Bakery': 1, 'Brickyard': 1, 'Clay Pit': 3, 'Cropland': 6, 'Grain Mill': 1, 'Iron Foundry': 1, 'Iron Mine': 5, 'Sawmill': 1, 'Woodcutter': 4, 'Hero\'s Mansion': 1 },
+    { Bakery: 1, Brickyard: 1, 'Clay Pit': 1, Cropland: 15, 'Grain Mill': 1, 'Iron Foundry': 1, 'Iron Mine': 1, Sawmill: 1, Woodcutter: 1, "Hero's Mansion": 1 },
+    { Bakery: 1, Brickyard: 1, 'Clay Pit': 3, Cropland: 9, 'Grain Mill': 1, 'Iron Foundry': 1, 'Iron Mine': 3, Sawmill: 1, Woodcutter: 3, "Hero's Mansion": 1 },
+    { Bakery: 1, Brickyard: 1, 'Clay Pit': 4, Cropland: 7, 'Grain Mill': 1, 'Iron Foundry': 1, 'Iron Mine': 4, Sawmill: 1, Woodcutter: 3, "Hero's Mansion": 1 },
+    { Bakery: 1, Brickyard: 1, 'Clay Pit': 3, Cropland: 7, 'Grain Mill': 1, 'Iron Foundry': 1, 'Iron Mine': 4, Sawmill: 1, Woodcutter: 4, "Hero's Mansion": 1 },
+    { Bakery: 1, Brickyard: 1, 'Clay Pit': 4, Cropland: 7, 'Grain Mill': 1, 'Iron Foundry': 1, 'Iron Mine': 3, Sawmill: 1, Woodcutter: 4, "Hero's Mansion": 1 },
+    { Bakery: 1, Brickyard: 1, 'Clay Pit': 4, Cropland: 6, 'Grain Mill': 1, 'Iron Foundry': 1, 'Iron Mine': 4, Sawmill: 1, Woodcutter: 4, "Hero's Mansion": 1 },
+    { Bakery: 1, Brickyard: 1, 'Clay Pit': 3, Cropland: 6, 'Grain Mill': 1, 'Iron Foundry': 1, 'Iron Mine': 4, Sawmill: 1, Woodcutter: 5, "Hero's Mansion": 1 },
+    { Bakery: 1, Brickyard: 1, 'Clay Pit': 4, Cropland: 6, 'Grain Mill': 1, 'Iron Foundry': 1, 'Iron Mine': 3, Sawmill: 1, Woodcutter: 5, "Hero's Mansion": 1 },
+    { Bakery: 1, Brickyard: 1, 'Clay Pit': 5, Cropland: 6, 'Grain Mill': 1, 'Iron Foundry': 1, 'Iron Mine': 4, Sawmill: 1, Woodcutter: 3, "Hero's Mansion": 1 },
+    { Bakery: 1, Brickyard: 1, 'Clay Pit': 5, Cropland: 6, 'Grain Mill': 1, 'Iron Foundry': 1, 'Iron Mine': 3, Sawmill: 1, Woodcutter: 4, "Hero's Mansion": 1 },
+    { Bakery: 1, Brickyard: 1, 'Clay Pit': 4, Cropland: 6, 'Grain Mill': 1, 'Iron Foundry': 1, 'Iron Mine': 5, Sawmill: 1, Woodcutter: 3, "Hero's Mansion": 1 },
+    { Bakery: 1, Brickyard: 1, 'Clay Pit': 3, Cropland: 6, 'Grain Mill': 1, 'Iron Foundry': 1, 'Iron Mine': 5, Sawmill: 1, Woodcutter: 4, "Hero's Mansion": 1 },
 ];
 
 const maxLevelsDefault = {
-    'Bakery': 5,
-    'Brickyard': 5,
+    Bakery: 5,
+    Brickyard: 5,
     'Clay Pit': 10,
-    'Cropland': 10,
+    Cropland: 10,
     'Grain Mill': 5,
     'Iron Foundry': 5,
     'Iron Mine': 10,
-    'Sawmill': 5,
-    'Woodcutter': 10,
-    'Hero\'s Mansion': 3,
+    Sawmill: 5,
+    Woodcutter: 10,
+    "Hero's Mansion": 3,
 };
 
 const herosMansionMap = {
     1: 10,
     2: 15,
     3: 20,
-}
+};
 
-var maxLevels = {};
-
-var buildingData = [];
+let maxLevels = {};
+let buildingData = [];
 const oases = [
-    { "wood": 0, "clay": 0, "iron": 0, "crop": 0 },
-    { "wood": 0, "clay": 0, "iron": 0.25, "crop": 0.25 },
-    { "wood": 0, "clay": 0, "iron": 0, "crop": 0 },
-    { "wood": 0, "clay": 0, "iron": 0, "crop": 0 },
+    { wood: 0, clay: 0, iron: 0, crop: 0 },
+    { wood: 0, clay: 0, iron: 0.25, crop: 0.25 },
+    { wood: 0, clay: 0, iron: 0, crop: 0 },
+    { wood: 0, clay: 0, iron: 0, crop: 0 },
 ];
 
-function deepcopy(obj) {
-    return JSON.parse(JSON.stringify(obj));
-}
+const deepcopy = (obj) => JSON.parse(JSON.stringify(obj));
 
-async function loadJson(fileName, index) {
-    return fetch(`data/${fileName}`)
-        .then(response => response.json())
-        .catch(error => console.error('Error loading JSON:', error));
-}
+const loadJson = async (fileName) => {
+    try {
+        const response = await fetch(`data/${fileName}`);
+        return await response.json();
+    } catch (error) {
+        console.error('Error loading JSON:', error);
+    }
+};
 
-function setupBuildings(villageType) {
-    buildings = []
-    for (const buildingType in buildingLimits[villageType]) {
-        const limit = buildingLimits[villageType][buildingType];
+const setupBuildings = (villageType) => {
+    const buildings = [];
+    for (const [buildingType, limit] of Object.entries(buildingLimits[villageType])) {
         for (let i = 0; i < limit; i++) {
             buildings.push({
                 type: buildingType,
                 level: 0,
                 paybackPeriod: Infinity,
-                canBeUpgraded: !['Bakery', 'Brickyard', 'Grain Mill', 'Iron Foundry', 'Sawmill'].includes(buildingType)
+                canBeUpgraded: !['Bakery', 'Brickyard', 'Grain Mill', 'Iron Foundry', 'Sawmill'].includes(buildingType),
             });
         }
     }
 
-    // Sort the buildings alphabetically based on the type
     buildings.sort((a, b) => a.type.localeCompare(b.type));
-
     return buildings;
-}
+};
 
-function calculateProduction(buildings, hasBonus = false) {
+const calculateProduction = (buildings, hasBonus = false) => {
     const bonus = hasBonus ? 1.25 : 1;
     let woodProduction = 0;
     let clayProduction = 0;
@@ -111,158 +107,129 @@ function calculateProduction(buildings, hasBonus = false) {
     let clayProductionMultiplier = 1;
     let ironProductionMultiplier = 1;
     let cropProductionMultiplier = 1;
-    let totalProduction = 0;
 
-    for (let buildingIndex in buildings) {
-        const building = buildings[buildingIndex];
-        if (building.type == "Woodcutter") {
-            woodProduction += buildingData[building.type][building.level].prod;
-        } else if (building.type == "Clay Pit") {
-            clayProduction += buildingData[building.type][building.level].prod;
-        } else if (building.type == "Iron Mine") {
-            ironProduction += buildingData[building.type][building.level].prod;
-        } else if (building.type == "Cropland") {
-            if(building.level == 23){
-                console.log(building);
-            }
-            cropProduction += buildingData[building.type][building.level].prod;
+    buildings.forEach((building) => {
+        const production = buildingData[building.type][building.level]?.prod || 0;
+        switch (building.type) {
+            case 'Woodcutter':
+                woodProduction += production;
+                break;
+            case 'Clay Pit':
+                clayProduction += production;
+                break;
+            case 'Iron Mine':
+                ironProduction += production;
+                break;
+            case 'Cropland':
+                cropProduction += production;
+                break;
+            case 'Sawmill':
+                woodProductionMultiplier += production;
+                break;
+            case 'Brickyard':
+                clayProductionMultiplier += production;
+                break;
+            case 'Iron Foundry':
+                ironProductionMultiplier += production;
+                break;
+            case 'Grain Mill':
+            case 'Bakery':
+                cropProductionMultiplier += production;
+                break;
+            case "Hero's Mansion":
+                for (let level = 0; level <= building.level; level++) {
+                    woodProductionMultiplier += oases[level].wood;
+                    clayProductionMultiplier += oases[level].clay;
+                    ironProductionMultiplier += oases[level].iron;
+                    cropProductionMultiplier += oases[level].crop;
+                }
+                break;
         }
-    }
-
-    for (let buildingIndex in buildings) {
-        const building = buildings[buildingIndex];
-        if (building.type == "Sawmill") {
-            woodProductionMultiplier += buildingData[building.type][building.level].prod;
-        } else if (building.type == "Brickyard") {
-            clayProductionMultiplier += buildingData[building.type][building.level].prod;
-        } else if (building.type == "Iron Foundry") {
-            ironProductionMultiplier += buildingData[building.type][building.level].prod;
-        } else if (building.type == "Grain Mill") {
-            cropProductionMultiplier += buildingData[building.type][building.level].prod;
-        } else if (building.type == "Bakery") {
-            cropProductionMultiplier += buildingData[building.type][building.level].prod;
-        }
-    }
-
-    for (let buildingIndex in buildings) {
-        const building = buildings[buildingIndex];
-        if (building.type == "Hero's Mansion") {
-            for (let level = 0; level <= building.level; level++) {
-                woodProductionMultiplier += oases[level].wood;
-                clayProductionMultiplier += oases[level].clay;
-                ironProductionMultiplier += oases[level].iron;
-                cropProductionMultiplier += oases[level].crop;
-            }
-        }
-    }
+    });
 
     woodProduction *= woodProductionMultiplier * bonus;
     clayProduction *= clayProductionMultiplier * bonus;
     ironProduction *= ironProductionMultiplier * bonus;
     cropProduction *= cropProductionMultiplier * bonus;
 
-    totalProduction = woodProduction + clayProduction + ironProduction + cropProduction;
+    return woodProduction + clayProduction + ironProduction + cropProduction;
+};
 
-    return totalProduction;
-}
-
-function calculatePaybackPeriods(buildings, hasBonus = false) {
+const calculatePaybackPeriods = (buildings, hasBonus = false) => {
     const currentProduction = calculateProduction(buildings, hasBonus);
-    buildings.forEach(building => {
-        if(!building.canBeUpgraded){
-            return;
-        }
+    buildings.forEach((building) => {
+        if (!building.canBeUpgraded) return;
         const currentLevel = building.level;
         building.level += 1;
         const newProduction = calculateProduction(buildings, hasBonus);
         building.level = currentLevel;
         const increase = newProduction - currentProduction;
-        const cost = buildingData[building.type][building.level + 1].total;
+        const cost = buildingData[building.type][building.level + 1]?.total || 0;
         const paybackHours = cost / increase;
         building.paybackPeriod = paybackHours;
     });
-}
+};
 
-function getBuildingWithLowestPaybackPeriod(buildings) {
+const getBuildingWithLowestPaybackPeriod = (buildings) => {
     calculatePaybackPeriods(buildings);
-    const minValidObject = buildings.reduce((min, obj) => {
-        if (!obj.canBeUpgraded) return min; // Skip invalid objects
+    return buildings.reduce((min, obj) => {
+        if (!obj.canBeUpgraded) return min;
         return (min === null || obj.paybackPeriod < min.paybackPeriod) ? obj : min;
     }, null);
-    return minValidObject;
-}
+};
 
-function isThereAnyBuildingToUpgrade(buildings) {
-    return buildings.some(building => building.canBeUpgraded == true);
-}
+const isThereAnyBuildingToUpgrade = (buildings) => buildings.some(building => building.canBeUpgraded);
 
-function setOasesValues(){
-    const wood1 = document.getElementById("wood1");
-    const clay1 = document.getElementById("clay1");
-    const iron1 = document.getElementById("iron1");
-    const crop1 = document.getElementById("crop1");
-    const wood2 = document.getElementById("wood2");
-    const clay2 = document.getElementById("clay2");
-    const iron2 = document.getElementById("iron2");
-    const crop2 = document.getElementById("crop2");
-    const wood3 = document.getElementById("wood3");
-    const clay3 = document.getElementById("clay3");
-    const iron3 = document.getElementById("iron3");
-    const crop3 = document.getElementById("crop3");
+const setOasesValues = () => {
+    oases[1].wood = parseFloat(document.getElementById("wood1").value);
+    oases[1].clay = parseFloat(document.getElementById("clay1").value);
+    oases[1].iron = parseFloat(document.getElementById("iron1").value);
+    oases[1].crop = parseFloat(document.getElementById("crop1").value);
+    oases[2].wood = parseFloat(document.getElementById("wood2").value);
+    oases[2].clay = parseFloat(document.getElementById("clay2").value);
+    oases[2].iron = parseFloat(document.getElementById("iron2").value);
+    oases[2].crop = parseFloat(document.getElementById("crop2").value);
+    oases[3].wood = parseFloat(document.getElementById("wood3").value);
+    oases[3].clay = parseFloat(document.getElementById("clay3").value);
+    oases[3].iron = parseFloat(document.getElementById("iron3").value);
+    oases[3].crop = parseFloat(document.getElementById("crop3").value);
+};
 
-    oases[1].wood = parseFloat(wood1.value);
-    oases[1].clay = parseFloat(clay1.value);
-    oases[1].iron = parseFloat(iron1.value);
-    oases[1].crop = parseFloat(crop1.value);
-    oases[2].wood = parseFloat(wood2.value);
-    oases[2].clay = parseFloat(clay2.value);
-    oases[2].iron = parseFloat(iron2.value);
-    oases[2].crop = parseFloat(crop2.value);
-    oases[3].wood = parseFloat(wood3.value);
-    oases[3].clay = parseFloat(clay3.value);
-    oases[3].iron = parseFloat(iron3.value);
-    oases[3].crop = parseFloat(crop3.value);
-}
-
-function writeData() {
+const writeData = () => {
     const content = document.getElementById('content');
     content.innerHTML = '';
-    const villageTypeField = document.getElementById("villageType");
-    const villageType = villageTypeField.value;
-    const isCapitalCheckbox = document.getElementById('isCapital');
-    const isCapital = isCapitalCheckbox.checked;
+    const villageType = document.getElementById("villageType").value;
+    const isCapital = document.getElementById('isCapital').checked;
 
-    maxLevels = structuredClone(maxLevelsDefault);
-    if(isCapital){
-        maxLevels['Woodcutter'] = 22;
+    maxLevels = { ...maxLevelsDefault };
+    if (isCapital) {
+        maxLevels.Woodcutter = 22;
         maxLevels['Clay Pit'] = 22;
         maxLevels['Iron Mine'] = 22;
-        maxLevels['Cropland'] = 22;
+        maxLevels.Cropland = 22;
     }
 
     setOasesValues();
-
     const buildings = setupBuildings(villageType);
+
     while (isThereAnyBuildingToUpgrade(buildings)) {
         const toUpgrade = getBuildingWithLowestPaybackPeriod(buildings);
         toUpgrade.level += 1;
-        if (toUpgrade.level == maxLevels[toUpgrade.type]) {
+        if (toUpgrade.level === maxLevels[toUpgrade.type]) {
             toUpgrade.canBeUpgraded = false;
         }
         const buildingName = toUpgrade.type;
-        const buildingLevel = buildingName == 'Hero\'s Mansion' ? herosMansionMap[toUpgrade.level] : toUpgrade.level;
+        const buildingLevel = buildingName === "Hero's Mansion" ? herosMansionMap[toUpgrade.level] : toUpgrade.level;
         content.innerHTML += `Upgrade ${buildingName} to ${buildingLevel}<br>`;
     }
-}
+};
 
-document.addEventListener("DOMContentLoaded", async function () {
+document.addEventListener("DOMContentLoaded", async () => {
     const promises = filenames.map(loadJson);
     const response = await Promise.all(promises);
-    for (let dataKey in response) {
-        const data = response[dataKey];
-        key = Object.keys(data)[0];
-        value = data[key];
+    response.forEach(data => {
+        const [key, value] = Object.entries(data)[0];
         buildingData[key] = value;
-    }
+    });
     document.getElementById("calculateButton").onclick = writeData;
 });
